8eea343e07ab345bf0cae82404e8c9a45fa8acb3
MI_C_00\EH_MI <nikisa97@gmail.com>
2019-03-21 18:18:23 +0100

    some features

---
+++ TankGp1/Assets/Scripts/Managers/InputSystem.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputSystem : MonoBehaviour
+{
+    TankView tankView;
+    
+    // Use this for initialization
+    void Start()
+    {
+        if (tankView == null)
+        {
+            tankView = GetComponent<TankView>(); 
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CheckInput();
+        Debug.Log(Input.GetAxis("movement"));
+    }
+
+    public void Movement(Movement _type)
+    {
+        switch (_type)
+        {
+            case global::Movement.Up:
+                transform.Translate(Vector3.forward);
+                break;
+            case global::Movement.Down:
+                transform.Translate(Vector3.back);
+                break;
+        }
+       
+    }
+
+    public void Rotation(Rotation _type)
+    {
+        switch (_type)
+        {
+            case global::Rotation.Left:
+                transform.Rotate(Vector3.down);
+                break;
+            case global::Rotation.Right:
+                transform.Rotate(Vector3.up);
+                break;
+           
+        }
+
+    }
+
+    public void CheckInput()
+    {
+        if (Input.GetAxis("movement") > 0)
+        {
+            Movement(global::Movement.Up);
+        }
+        if (Input.GetAxis("movement") < 0)
+        {
+            Movement(global::Movement.Down);
+        }
+
+        if (Input.GetAxis("rotation") > 0)
+        {
+            Rotation(global::Rotation.Right);
+        }
+        else if (Input.GetAxis("rotation") < 0)
+        {
+            Rotation(global::Rotation.Left);
+        }
+    }
+}
+public enum Movement
+{
+    Up,
+    Down,
+    
+}
+
+public enum Rotation
+{
+    Left, 
+    Right,
+}
---
+++ TankGp1/Assets/Scripts/Projectile/BaseProjectile.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseProjectile : MonoBehaviour, IDestructable
+{
+
+    public int Damage;
+    public float Speed;
+
+}
